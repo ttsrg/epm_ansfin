2780ddc5e2d4d3c73f370b476b5f3a62ee8889aa
root <root@EPBYMINW2629.minsk.epam.com>
2019-04-04 10:28:49 +0300

    for serge

--- start.sh
+++ start.sh
@@ -1,2 +1,3 @@
 export ANSIBLE_STDOUT_CALLBACK=debug
-ansible-playbook -i ~/ansible/epm_ansfin/inventory/stage  ~/ansible/epm_ansfin/playbook.yml --tags=nexus
+ansible-playbook -i ~/ansible/epm_ansfin/inventory/stage  ~/ansible/epm_ansfin/playbook.yml 
+#--tags=nexus
